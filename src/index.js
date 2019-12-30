fb83c32b25d42a5858091b200c0707d344cc6ef1
Rui Gil <ruipgil@users.noreply.github.com>
2019-12-30 17:17:44 +0000

    Update documentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const zeroJitter = () => 0
 
+/**
+ * Throttle execution of functions
+ *
+ * @param  {!Array<function:Promise>} functions Funtions to execute
+ * @param  {Object} options Options object. In the form:
+ *  {
+ *    jitter: Function(Number=, Number=),
+ *    interval: Number,
+ *    concurrency: Number
+ *  }
+ *  where:
+ *  - jitter: is a function that returns a number, it will be
+ *    used to delay the execution of the function. The default
+ *    function no jitter
+ *  - interval: minimum number of milliseconds between requests
+ *  - concurrency: maximum number of function to execute concurrently.
+ *    Note that function that Javascript doesn't allow the execution
+ *    of concurrent functions. In fact the concurrency refers to the
+ *    number of promises being execute. Imagine that you set the
+ *    concurrency to 2 and pass an array of functions to execute,
+ *    that will make requests to webpages. With `f-throttler` you are
+ *    able to have two requests at the same time, altough the code
+ *    that handles it will run one at a time.
+ */
 const throttler = (promises, { jitter = zeroJitter, concurrency = 1, interval = 0 }) => new Promise((resolve, reject) => {
   const { length } = promises
 
